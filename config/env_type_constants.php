9080b53ac9514769d2a417daf0ee8a7b33ae3bcb
JoseModiUon <166022304+JoseModiUon@users.noreply.github.com>
2024-08-12 15:48:08 +0300

    Add files via upload

---
+++ config/env_type_constants.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Rufusy Idachi <idachirufus@gmail.com>
+ * @date: 3/18/2024
+ * @time: 10:59 AM
+ */
+
+// Here you put debug and env settings to be used that are specific to the environment type. i.e. prod or dev
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
