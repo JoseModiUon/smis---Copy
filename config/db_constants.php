0683d1176d995aa7fe3156d0b4b734533d670cd3
JoseModiUon <166022304+JoseModiUon@users.noreply.github.com>
2024-08-12 15:53:53 +0300

    Add files via upload

---
+++ config/db_constants.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @author Rufusy Idachi <idachirufus@gmail.com>
+ */
+
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'dev');
+
+defined('SMIS_DB_SERVER') or define('SMIS_DB_SERVER', '41.89.92.189');
+defined('SMIS_DB_PORT') or define('SMIS_DB_PORT', '5432');
+defined('SMIS_DB_NAME') or define('SMIS_DB_NAME', 'devdb1');
+defined('SMIS_DB_SCHEMA') or define('SMIS_DB_SCHEMA', 'smis');
+defined('SMIS_DB_USER') or define('SMIS_DB_USER', 'smisdev');
+defined('SMIS_DB_PASS') or define('SMIS_DB_PASS', 'SM1sAdmin23');
+
+defined('SMIS_PORTAL_DB_SERVER') or define('SMIS_PORTAL_DB_SERVER', '41.89.92.189');
+defined('SMIS_PORTAL_DB_PORT') or define('SMIS_PORTAL_DB_PORT', '5432');
+defined('SMIS_PORTAL_DB_NAME') or define('SMIS_PORTAL_DB_NAME', 'devdb2');
+defined('SMIS_PORTAL_DB_SCHEMA') or define('SMIS_PORTAL_DB_SCHEMA', 'smisportal');
+defined('SMIS_PORTAL_DB_USER') or define('SMIS_PORTAL_DB_USER', 'smisportal');
+defined('SMIS_PORTAL_DB_PASS') or define('SMIS_PORTAL_DB_PASS', 'P0rt@ladmin23');
